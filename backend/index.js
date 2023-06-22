90e017d49593339e0e432161bdca1045e6fb91d9
abdoOmar01 <abdoomar3001@gmail.com>
2023-06-22 19:06:39 +0300

    added python command

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const ytdl = require('ytdl-core')
 const fs = require('fs')
+const { exec } = require('child_process')
 const cors = require('cors')
 
 const requestLogger = (req, res, next) => {
@@ -27,6 +28,17 @@ app.get('/fetch', (req, res) => {
     ytdl(url, {
         format: 'mp4'
     }).pipe(fs.createWriteStream('../../Video-Ctrl-F/data/video.mp4'))
+
+    exec('conda run -n GP python ../../Video-Ctrl-F/src/main.py ../data/video.mp4',
+        (error, stdout, stderr) => {
+        if (error) {
+            console.log(`Error: ${error}`)
+            return
+        }
+        
+        console.log(`Stdout: ${stdout}`)
+        console.log(`Stderr: ${stderr}`)
+    })
 
     console.log(`Video downloaded: ${url}`)
 })
